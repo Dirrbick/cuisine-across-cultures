7ba92cb3b497c4c5c9bd5fb901487df15ff3438b
dirrbick <kory.a.jaxn@gmail.com>
2019-09-21 17:29:08 -0700

    added javascript to the files

---
+++ script.js
@@ -0,0 +1,17 @@
+'use strict';
+
+var username = prompt('Welcome! What is your name?');
+
+console.log(username);
+console.log('Hi, ' + username + '!');
+
+
+document.write('<p>Hi, ' + username + '<p>');
+
+var wantToEat = prompt('We are so glad you are hear. Are you new to different cuisines?');
+
+if (wantToEat === 'Yes'){
+  document.write('<p>We are so excited! Head over to one of the regions to find out more.</p>');
+} else {
+  document.write('<p>Thank you for stopping by, we hope you can enjoy some of the dishes!</p>');
+}
