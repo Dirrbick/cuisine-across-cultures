cc2b9f7248a038d7c680644da1c2a72e4a884520
dirrbick <kory.a.jaxn@gmail.com>
2019-09-24 22:30:12 -0700

    tweaks to css

--- createRecipe.js
+++ createRecipe.js
@@ -3,12 +3,16 @@
 // work:v
 // output:"<article class=burger>
 
-function createRecipe (name, origin, ingredients) {
+function createRecipe (name, time,origin, ingredients) {
   var newRecipe = '<article>' +
         '<img src=burger.jpg>' +
         '<h3>' +
         name +
         '</h3>' +
+        '<li>' +
+        'Cook Time: ' +
+        time +
+        '</li>' +
         '<li>' +
         origin +
         '</li>' +
@@ -23,12 +27,12 @@ var wantsToAddRecipe = prompt('Do you want to add new recipe?');
 
 if (wantsToAddRecipe === 'Yes') {
   var recipeName = prompt('What is the recipe name?');
+  var recipeTime = prompt('What is the total cook time?');
   var recipeOrigin = prompt('Which region is this from?');
   var recipeIngredients = prompt('Please list the ingredients.');
 
-  var newRecipeHTML = createRecipe(recipeName, recipeOrigin, recipeIngredients);
+  var newRecipeHTML = createRecipe(recipeName, recipeTime, recipeOrigin, recipeIngredients);
 
   document.write(newRecipeHTML);
-
-
 }
+
